94a6b2ea86389aedb80c74239c769cc1eb41fc9e
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    nemo: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- device.mk
+++ device.mk
@@ -117,7 +117,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.nemo
+    android.hardware.light-service.lineage
 
 # Lineage Health
 PRODUCT_PACKAGES += \
