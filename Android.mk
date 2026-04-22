a3ce5c0af9a9e967a7ce63ec8ff91dc550578d72
Debayan Kar <kardebayan3@gmail.com>
2026-04-22 12:53:59 +0000

    Revert "nemo: Remove oppo logical partitions when necessary"
    This reverts commit 12b2abeffe6a23b5ca293efbbfb70c0ac5882a56.
    
    Change-Id: I3004aa9d0e21812af88ef44e3ed53cf381bd7157

--- Android.mk
+++ Android.mk
@@ -8,7 +8,6 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),nemo)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(call add-radio-file,dynamic-remove-oppo)
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 include $(CLEAR_VARS)
